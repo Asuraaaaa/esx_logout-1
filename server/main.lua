140e85f656eccb4b18b9df9a11433e199f6cfb5e
MrFr4g <fragmatheo@gmail.com>
2020-04-02 08:37:07 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -4,21 +4,15 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent("esx_logout:save")
 AddEventHandler("esx_logout:save", function()
-    local _source = source
+    	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	
-	local money = xPlayer.getMoney()
-	local bank = xPlayer.getAccount('bank').money
-	
-	print(money)
-	print(bank)
-	
 	ESX.SavePlayer(xPlayer)
-		MySQL.Async.execute('UPDATE users SET `money` = @money, `bank` = @bank WHERE identifier = @identifier', {
-			['@money']   = xPlayer.getMoney(),
-			['@bank']   = xPlayer.getAccount('bank').money,
-			['@identifier'] = xPlayer.identifier
-		})
+	MySQL.Async.execute('UPDATE users SET `money` = @money, `bank` = @bank WHERE identifier = @identifier', {
+		['@money']   = xPlayer.getMoney(),
+		['@bank']   = xPlayer.getAccount('bank').money,
+		['@identifier'] = xPlayer.identifier
+	})
 end)
 
 ESX.RegisterServerCallback('esx_logout:getUsergroup', function(source, cb)
